b5116eba5b8f4f0e6a7e8dc5564a6b14044991c3
Felipe Moreira <fmoreira95@hotmail.com>
2021-05-10 15:25:43 -0400

    Mobile menu icon functionality complete

--- script/script.js
+++ script/script.js
@@ -1,4 +1,6 @@
 const arrowButton = document.querySelectorAll(".menu-arrow");
+const hamburgerBtn = document.getElementById("hamburger");
+const navLink = document.querySelector(".nav__links");
 
 arrowButton.forEach((el) =>
   el.addEventListener("click", (event) => {
@@ -6,3 +8,8 @@ arrowButton.forEach((el) =>
     subMenu.classList.toggle("open");
   })
 );
+
+navLink.style.left = "-100%";
+hamburgerBtn.addEventListener("click", () => {
+  navLink.style.left = navLink.style.left === "-100%" ? "0" : "-100%";
+});
