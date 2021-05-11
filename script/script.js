b6f5537223c193f1b76c94027939e236839c2a21
Felipe Moreira <fmoreira95@hotmail.com>
2021-05-10 20:10:00 -0400

    future container desktop styled

--- script/script.js
+++ script/script.js
@@ -1,6 +1,7 @@
 const arrowButton = document.querySelectorAll(".menu-arrow");
 const hamburgerBtn = document.getElementById("hamburger");
 const navLink = document.querySelector(".nav__links");
+const reswidth = screen.width;
 
 arrowButton.forEach((el) =>
   el.addEventListener("click", (event) => {
@@ -13,3 +14,15 @@ navLink.style.left = "-100%";
 hamburgerBtn.addEventListener("click", () => {
   navLink.style.left = navLink.style.left === "-100%" ? "0" : "-100%";
 });
+
+function resizeImg() {
+  const reswidth = screen.width;
+  let image = document.querySelector(".future__container--img");
+  console.log(image.src);
+  if (reswidth >= 1440) {
+    image.src = "../images/illustration-editor-desktop.svg";
+  } else {
+    image.src = "../images/illustration-editor-mobile.svg";
+  }
+}
+window.addEventListener("resize", resizeImg);
