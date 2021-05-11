34b9a8686d0a72cbafc8e4314d140f25879760cc
Felipe Moreira <fmoreira95@hotmail.com>
2021-05-10 23:48:55 -0400

    Styling state of the art section

--- script/script.js
+++ script/script.js
@@ -15,14 +15,24 @@ hamburgerBtn.addEventListener("click", () => {
   navLink.style.left = navLink.style.left === "-100%" ? "0" : "-100%";
 });
 
-function resizeImg() {
+const resizeEditiorImg = () => {
   const reswidth = screen.width;
   let image = document.querySelector(".future__container--img");
-  console.log(image.src);
   if (reswidth >= 1440) {
     image.src = "../images/illustration-editor-desktop.svg";
   } else {
     image.src = "../images/illustration-editor-mobile.svg";
   }
-}
-window.addEventListener("resize", resizeImg);
+};
+window.addEventListener("resize", resizeEditiorImg);
+
+const resizeLaptopImg = () => {
+  const reswidth = screen.width;
+  let image = document.querySelector(".free__container--img");
+  if (reswidth >= 1440) {
+    image.src = "../images/illustration-laptop-desktop.svg";
+  } else {
+    image.src = "../images/illustration-laptop-mobile.svg";
+  }
+};
+window.addEventListener("resize", resizeLaptopImg);
